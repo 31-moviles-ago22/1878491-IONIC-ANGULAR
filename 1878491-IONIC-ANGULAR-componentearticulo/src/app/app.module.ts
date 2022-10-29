6ea7184d4d59793c8728bec7bbf45ac51779b7a6
Alexis121000 <105323426+Alexis121000@users.noreply.github.com>
2022-10-29 01:33:33 -0500

    componente-articulo

---
+++ 1878491-IONIC-ANGULAR-componentearticulo/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { IonicModule } from '@ionic/angular';
+import { ArticulosComponent } from './articulos/articulos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    ArticulosComponent
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
